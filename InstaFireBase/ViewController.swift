a44d4c7c659cb5ee8ca3f4c2bed71f63ca420327
Patrik Rikama <patrik.rikama@gmail.com>
2018-10-08 11:24:54 +0200

    Initial Commit

---
+++ InstaFireBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaFireBase
+//
+//  Created by Patrik Rikama Hinnenberg on 2018-10-08.
+//  Copyright © 2018 Patrik Rikama Hinnenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
