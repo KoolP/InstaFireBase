db63d49be577b8bf3b3f4517a8dfd8ed1148fca8
Patrik Rikama <patrik.rikama@gmail.com>
2018-10-08 14:46:58 +0200

    start

--- InstaFireBase/ViewController.swift
+++ InstaFireBase/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let plusPhotoButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .red
+        button.setImage(user_image, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        view.addSubview(plusPhotoButton)
+//        plusPhotoButton.frame = CGRect(x: 0, y: 0, width: 140, height: 140)
+//        plusPhotoButton.center = view.center
+        //Autolayout
+        plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        
+        
+        
     }
 
 
